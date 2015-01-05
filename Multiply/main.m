6666e3f60982ce61fc2ad05e13c236f3d486b06f
Don Wettasinghe <donwettasinghe@Dons-MacBook-Pro.local>
2015-01-05 14:28:21 -0500

    Initial Commit

---
+++ Multiply/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Multiply
+//
+//  Created by Don Wettasinghe on 1/5/15.
+//  Copyright (c) 2015 Don Wettasinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
